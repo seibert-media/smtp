99ad61a932e06b61fa9a247823082b98c5860a74
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-24 23:09:35 +0100

    use template

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
+smtp_host=${SMTP_HOST:-'iredmail.mailfolder.org'}
+smtp_port=${SMTP_PORT:-'25'}
+
 opts=(
 	dc_local_interfaces '0.0.0.0 ; ::0'
 	dc_other_hostnames ''
 	dc_relay_nets "10.101.0.0/16:10.102.0.0/16:$(ip addr show dev eth0 | awk '$1 == "inet" { print $2 }')"
 	dc_eximconfig_configtype 'smarthost'
-	dc_smarthost 'iredmail.mailfolder.org:25'
+	dc_smarthost "${smtp_host}:${smtp_port}"
 	dc_relay_domains ''
 )
 

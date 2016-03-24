df33c0280940d309664776192bd522a3afaf5aee
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-24 14:15:12 +0100

    add relay nets

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 opts=(
 	dc_local_interfaces '0.0.0.0 ; ::0'
 	dc_other_hostnames ''
-	dc_relay_nets "$(ip addr show dev eth0 | awk '$1 == "inet" { print $2 }')"
+	dc_relay_nets "10.101.0.0/16:10.102.0.0/16:$(ip addr show dev eth0 | awk '$1 == "inet" { print $2 }')"
 	dc_eximconfig_configtype 'smarthost'
 	dc_smarthost 'iredmail.mailfolder.org:25'
 	dc_relay_domains ''

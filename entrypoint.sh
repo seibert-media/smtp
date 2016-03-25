4488a53d3d0ede2256d8f9815cb905906e3b2621
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-25 15:59:39 +0100

    add dns

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ SERVER_HOSTNAME=${SERVER_HOSTNAME:-'smtp.default.svc.cluster.local'}
 DOMAIN=${DOMAIN:-'default.svc.cluster.local'}
 RELAY_NETWORKS=${RELAY_NETWORKS:-'192.168.0.0/16 127.0.0.0/8'}
 
+echo "nameserver 172.16.30.1" > /var/spool/postfix/etc/resolv.conf
+
 #Comment default mydestination, we will set it bellow
 sed -i -e '/mydestination/ s/^#*/#/' /etc/postfix/main.cf
 sed -i -e 's/inet_interfaces = localhost/inet_interfaces = all/g' /etc/postfix/main.cf
@@ -19,5 +21,6 @@ echo 'myorigin=$mydomain'  >> /etc/postfix/main.cf
 echo "relayhost=[$SMTP_HOST]:${SMTP_PORT}" >> /etc/postfix/main.cf
 echo "mynetworks=${RELAY_NETWORKS}" >> /etc/postfix/main.cf
 echo "smtp_sasl_auth_enable = no" >> /etc/postfix/main.cf
+echo "smtpd_use_tls = no" >> /etc/postfix/main.cf
 
 exec "$@"

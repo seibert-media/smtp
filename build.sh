d39f8890448de2b2e7c1c6b8ef7afcecd8eac3c3
Benjamin Borbe <bborbe@nova.fritz.box>
2017-01-27 19:24:05 +0100

    update build

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 set -e
 
 tag=$(git tag -l --points-at HEAD)
-if ! test -z "$tag"; then
+if [ ! -z "$tag" ]; then
 	VERSION=${tag} make build
 	VERSION=${tag} make upload
 	VERSION=${tag} make clean
 fi
 
 branch=$(git rev-parse --abbrev-ref HEAD)
-if ! "$branch" = "HEAD"; then
+if [ ! "$branch" = "HEAD" ]; then
 	VERSION=${branch} make build
 	VERSION=${branch} make upload
 	VERSION=${branch} make clean
